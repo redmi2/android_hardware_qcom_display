03d9c30af4bed200911b55e8374c7c8ef8f5cad9
Steve Kondik <shade@chemlab.org>
2014-11-10 13:02:22 -0800

    Don't build lights module if vendor supplies it
    Change-Id: I63589281961e1120e7536798df6d78b8a7af4304
    Signed-off-by: Louis Popi <theh2o64@gmail.com>
    
    Conflicts:
    	Android.mk
    
    Change-Id: Ic069de66a1ded80f5a71a96265d97899f5e3f4bc

--- Android.mk
+++ Android.mk
@@ -1,9 +1,12 @@
-display-hals := libgralloc libgenlock libcopybit liblight
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libhdmi libqservice
 display-hals += libmemtrack
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
